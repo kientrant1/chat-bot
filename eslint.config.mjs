e91733b1a1ff8ca1100bda60658fc4bc95ff8080
Kien Tran <>
2025-10-28 10:30:10 +0700

    ci: update configuration files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,21 @@
-import { defineConfig, globalIgnores } from "eslint/config"
-import nextVitals from "eslint-config-next/core-web-vitals"
-import nextTs from "eslint-config-next/typescript"
+import { defineConfig } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettierConfig,
   {
+    plugins: {
+      prettier,
+    },
     rules: {
-      "semi": ["error", "never"],
+      'prettier/prettier': 'error',
     },
   },
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
 ])
 
 export default eslintConfig
