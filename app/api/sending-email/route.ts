383bff267ab0feab6c83ec25129037715404c98b
KT <>
2025-11-20 12:02:50 +0700

    fix: build error

--- app/api/sending-email/route.ts
+++ app/api/sending-email/route.ts
@@ -3,9 +3,8 @@ import { Resend } from 'resend'
 import logger from '@/utils/logger'
 import { Email } from '@/types/email'
 
-const resend = new Resend(process.env.RESEND_API_KEY)
-
 export async function POST(req: Request) {
+  const resend = new Resend(process.env.RESEND_API_KEY)
   try {
     const body: Email = await req.json()
 
